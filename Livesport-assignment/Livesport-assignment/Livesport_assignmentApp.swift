3d13c320f73f07e59a850cf03803ce19663efdc4
Alex Solonevich <soalsec@gmail.com>
2023-07-31 15:20:58 +0200

    initial commit

---
+++ Livesport-assignment/Livesport-assignment/Livesport_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Livesport_assignmentApp.swift
+//  Livesport-assignment
+//
+//  Created by Alex Solonevich on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Livesport_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
